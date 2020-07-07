6ed8993cbe853e97047cad60d148a14247fd7366
huy8895 <huy8895@gmail.com>
2020-07-07 11:30:56 +0700

    clean code

--- src/Gop_mang/MergingArray.java
+++ src/Gop_mang/MergingArray.java
@@ -8,21 +8,16 @@ public class MergingArray {
     public static void main(String[] args) {
         int[] arr1 = AddElementToArray.createRandomArray();
         int[] arr2 = AddElementToArray.createRandomArray();
-        int[] arr3 = new int[20];
+        int[] arr3 = new int[arr1.length + arr2.length];
         System.out.println("Array 3: ");
-        for (int i = 0; i < arr3.length;i++){
-            if (i < arr1.length){
+        for (int i = 0; i < arr3.length; i++) {
+            if (i < arr1.length) {
                 arr3[i] = arr1[i];
             }
-            if (i >= arr1.length){
-                arr3[i] = arr2[i - arr2.length];
+            if (i >= arr1.length) {
+                arr3[i] = arr2[i - arr1.length];
             }
             System.out.print(arr3[i] + "\t");
         }
-
-
-
     }
-
-
 }
